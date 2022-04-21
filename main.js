33726e6d1763ef84e98f6f707ceb2409807ecdf9
jacintha <jacintha.h@gmail.com>
2022-04-21 13:49:38 +0200

    UI Version 4

--- main.js
+++ main.js
@@ -1,69 +1,115 @@
-
-
-// const onClick = () => {
-//     console.log(this.id, this.innerHTML);
-//     document.getElementById('display').innerHTML = "test";
-//  }
-
 const allButtons = document.querySelectorAll(".button");
+let displayInput = "";
+let clickedButtonValue = "";
+let isDecimal = false;
+let isNegative = false;
 console.log(allButtons);
-const simpleOperators = document.querySelectorAll(".button--orange");
-console.log(simpleOperators);
-const otherOperators = document.querySelectorAll(".button--white");
-console.log(otherOperators);
-const zero = document.querySelector(".button--oval");
-console.log(zero);
-const decimalPoint = document.querySelector(".button--decimal");
-console.log(decimalPoint);
-const numbers = document.querySelectorAll(".button--number");
-console.log(numbers);
 
-let displayInput = ''
-   
-zero.onclick = function(_event) { 
-    console.log(zero);         
-    displayInput = displayInput + (this.innerHTML);
-    document.getElementById("input").innerHTML = displayInput ;
+
+
+
+
+for (let i = 0; i < allButtons.length; i++) {
+  const clickedButton = allButtons[i];
+  clickedButton.onclick = function (_event) {
+    //console.log(clickedButton);
+    //displayInput = displayInput + this.innerHTML;
+    //console.log(this.innerHTML);
+    clickedButtonValue = this.innerHTML;
+    console.log(clickedButtonValue);
+
+    //maximum length of the input number value = 10
+
+
+    switch (clickedButtonValue) { 
+
+      // Other Operators or special operators ' C ' , ' +/- ' ,  ' % '
+      case "C":  // COMPLETED
+        console.log("It is a c");
+        isDecimal = false;
+        isNegative = false;
+        displayInput = "";
+         // clear all: ip, op, flags   
+         //?? clickedButtonValue = "";
+        displayInput = displayInput;
+        break;
+
+      case "+/-": // COMPLETED
+        console.log("It is a +-");         // initialise flag &  // toggle flag
+        if (isNegative === false) {
+          displayInput = '-' + displayInput;
+          isNegative = true;
+        } else {
+          displayInput = displayInput.slice(1);
+          isNegative = false;
+        }
+        break;
+
+      case "%": // TO DO
+        console.log("It is a %");
+        displayInput +=  this.innerHTML;
+        break;
+
+        // simple number operators  ' + ' , ' - ' ,  ' * ' , ' / ' , '  =  '
+
+        //let lastkey = displayInput.slice(-1) : function  when operator is pressed was the previous key an operator as well ? replace it then
+        // new numeric variable to hold the numbers keyed in before the operator button is pressed
+        // function to calculate the (reduce) calculation expression
+        // = symbol rules
+        //output display rules & variable updates acc to each function
+
+      case "+":
+        console.log("It is a +");
+        displayInput = displayInput + this.innerHTML;
+        break;
+
+      case "-":
+        console.log("It is a -");
+        displayInput = displayInput + this.innerHTML;
+        break;
+
+      case "x":
+        console.log("It is a x");
+        displayInput = displayInput + this.innerHTML;
+        break;
+
+      case "÷":
+        console.log("It is a ÷");
+        displayInput = displayInput + this.innerHTML;
+        break;
+
+      case "=":
+        console.log("It is a =");
+        displayOutput = displayInput;
+        break;
+
+      case ".": // COMPLETED
+        console.log("It is a ."); //initialise flag, set flag, if flag on : ignore
+        if (isDecimal === false) {
+          if(displayInput === ''){displayInput = 0} // prefix 0 if there is no number before the decimal point
+            displayInput = displayInput + this.innerHTML;
+            isDecimal = true;
+        }   
+        break;
+
+         // NUMBERS ' 0 - 9 '
+
+      default:
+        console.log("It is a number");
+        //console.log(`check1 ${displayInput}`);
+        if (this.innerHTML === '0' && displayInput === '') {
+          displayInput = displayInput 
+         // console.log(`check2 ${displayInput}`);
+        } 
+        else { displayInput = displayInput + this.innerHTML
+         // console.log(`check3 ${displayInput}`);
+        };
+         // console.log(`check4 ${displayInput}`);
+        break;
+    }
+
+    document.getElementById("input").innerHTML = displayInput;
+    document.getElementById("output").innerHTML = displayOutput;
     console.log(displayInput);
-}
-   
- 
-
-decimalPoint.onclick = function(_event) { 
-    console.log(decimalPoint);   
-    displayInput = displayInput + (this.innerHTML);
-    document.getElementById("input").innerHTML = displayInput ;
-    console.log(displayInput);
-}
-
-for (let i = 0; i < numbers.length; i++) {
-    const number = numbers[i];
-    number.onclick = function(_event) { 
-        console.log(number);   
-        displayInput = displayInput + (this.innerHTML);
-        document.getElementById("input").innerHTML = displayInput ;
-        console.log(displayInput);
-    }
-    
-}
-
-for (let i = 0; i < simpleOperators.length; i++) {
-    const operator = simpleOperators[i];
-    operator.onclick = function(_event) { 
-        console.log(operator);   
-        displayInput = displayInput + (this.innerHTML);
-        document.getElementById("input").innerHTML = displayInput ;
-        console.log(displayInput);
-    }
-    
-}
-for (let i = 0; i < otherOperators.length; i++) {
-    const specialOperator = otherOperators[i];
-    specialOperator.onclick = function(_event) { 
-        console.log(specialOperator);   
-        displayInput = displayInput + (this.innerHTML);
-        document.getElementById("input").innerHTML = displayInput ;
-        console.log(displayInput);
-    }
-    
+  };
 }
--- test.js
+++ test.js
@@ -1,47 +1,147 @@
+
+const fruits ='banana';
+switch (fruits) {
+  case 'banana':
+    console.log('It is a banana')
+    break;
+
+  default:
+    console.log('It is not a banana')
+    break;
+}
+
+
+
+// // const onClick = () => {
+// //     console.log(this.id, this.innerHTML);
+// //     document.getElementById('display').innerHTML = "test";
+// //  }
+
+// const allButtons = document.querySelectorAll(".button");
+// console.log(allButtons);
+// const SimpleOperators = document.querySelectorAll(".button--orange");
+// console.log(SimpleOperators);
+// const OtherOperators = document.querySelectorAll(".button--white");
+// console.log(OtherOperators);
+// const zero = document.querySelector(".button--oval");
+// console.log(zero);
+// const decimalPoint = document.querySelector(".button--decimal");
+// console.log(decimalPoint);
+// const numbers = document.querySelectorAll(".button--number");
+// console.log(numbers);
+
+// const stringDigits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// //console.log(stringDigits)
+// const numberDigits = stringDigits.map((str) => parseInt(str));
+// //console.log(numberDigits)
+// const getElementById = numberDigits.map((num) => document.getElementById(num));
+// //console.log(getElementById)
+
+// let displayInput = "";
+
+// const element = getElementById.forEach((digit) => {
+//   digit.onclick = function (_event) {
+//     //console.log(element);
+//     //console.log(this.id, this.innerHTML);
+//     //document.getElementById("input").innerHTML = this.innerHTML ;
+
+//     displayInput = displayInput + this.innerHTML;
+//     document.getElementById("input").innerHTML = displayInput;
+//     console.log(displayInput);
+//   };
+// });
+
+// // //const element = document.getElementById("1");
+// // element.onclick = function(_event) {
+// //   console.log(element);
+// //   console.log(this.id, this.innerHTML);
+// //   document.getElementById("input").innerHTML = this.innerHTML ;
+// // }
+
+// // document.getElementById(id).onclick = function(){code}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const fruits = "apple";
+//console.log(clickedButtonValue);
+
 // const onClick = () => {
 //     console.log(this.id, this.innerHTML);
 //     document.getElementById('display').innerHTML = "test";
 //  }
 
-const allButtons = document.querySelectorAll(".button");
-console.log(allButtons);
-const SimpleOperators = document.querySelectorAll(".button--orange");
-console.log(SimpleOperators);
-const OtherOperators = document.querySelectorAll(".button--white");
-console.log(OtherOperators);
-const zero = document.querySelector(".button--oval");
-console.log(zero);
-const decimalPoint = document.querySelector(".button--decimal");
-console.log(decimalPoint);
-const numbers = document.querySelectorAll(".button--number");
-console.log(numbers);
-
-const stringDigits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-//console.log(stringDigits)
-const numberDigits = stringDigits.map((str) => parseInt(str));
-//console.log(numberDigits)
-const getElementById = numberDigits.map((num) => document.getElementById(num));
-//console.log(getElementById)
-
-let displayInput = "";
-
-const element = getElementById.forEach((digit) => {
-  digit.onclick = function (_event) {
-    //console.log(element);
-    //console.log(this.id, this.innerHTML);
-    //document.getElementById("input").innerHTML = this.innerHTML ;
-
-    displayInput = displayInput + this.innerHTML;
-    document.getElementById("input").innerHTML = displayInput;
-    console.log(displayInput);
-  };
-});
-
-// //const element = document.getElementById("1");
-// element.onclick = function(_event) {
-//   console.log(element);
-//   console.log(this.id, this.innerHTML);
-//   document.getElementById("input").innerHTML = this.innerHTML ;
+// const simpleOperators = document.querySelectorAll(".button--orange");
+// console.log(simpleOperators);
+// const otherOperators = document.querySelectorAll(".button--white");
+// console.log(otherOperators);
+// const zero = document.querySelector(".button--oval");
+// console.log(zero);
+// const decimalPoint = document.querySelector(".button--decimal");
+// console.log(decimalPoint);
+// const numbers = document.querySelectorAll(".button--number");
+// console.log(numbers);
+// allButtons.zero.onclick = function(_event) {
+//     console.log(zero);
+//     displayInput = displayInput + (this.innerHTML);
+//     document.getElementById("input").innerHTML = displayInput ;
+//     console.log(displayInput);
 // }
 
-// document.getElementById(id).onclick = function(){code}
+// zero.onclick = function(_event) {
+//     console.log(zero);
+//     displayInput = displayInput + (this.innerHTML);
+//     document.getElementById("input").innerHTML = displayInput ;
+//     console.log(displayInput);
+// }
+
+// decimalPoint.onclick = function(_event) {
+//     console.log(decimalPoint);
+//     displayInput = displayInput + (this.innerHTML);
+//     document.getElementById("input").innerHTML = displayInput ;
+//     console.log(displayInput);
+// }
+
+// for (let i = 0; i < numbers.length; i++) {
+//     const number = numbers[i];
+//     number.onclick = function(_event) {
+//         console.log(number);
+//         displayInput = displayInput + (this.innerHTML);
+//         document.getElementById("input").innerHTML = displayInput ;
+//         console.log(displayInput);
+//     }
+
+// }
+
+// for (let i = 0; i < simpleOperators.length; i++) {
+//     const operator = simpleOperators[i];
+//     operator.onclick = function(_event) {
+//         console.log(operator);
+//         displayInput = displayInput + (this.innerHTML);
+//         document.getElementById("input").innerHTML = displayInput ;
+//         console.log(displayInput);
+//     }
+
+// }
+// for (let i = 0; i < otherOperators.length; i++) {
+//     const specialOperator = otherOperators[i];
+//     specialOperator.onclick = function(_event) {
+//         console.log(specialOperator);
+//         displayInput = displayInput + (this.innerHTML);
+//         document.getElementById("input").innerHTML = displayInput ;
+//         console.log(displayInput);
+//     }
+
+// }
+
