313ef2d31ad0dbcbc457011d4724904eeefe3bb7
jacintha <jacintha.h@gmail.com>
2022-04-22 00:05:17 +0200

    UI Version 5

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
+
+
 const allButtons = document.querySelectorAll(".button");
-let displayInput = "";
+let numberDisplay = "";
+let calculationDisplay = "";
 let clickedButtonValue = "";
 let isDecimal = false;
 let isNegative = false;
-console.log(allButtons);
+//console.log(allButtons);
+let value1 = 0;
+let value2 = 0;
+let operator = '';
+let intNumberDisplay = 0;
+
 
 
 
@@ -11,105 +19,119 @@ console.log(allButtons);
 
 for (let i = 0; i < allButtons.length; i++) {
   const clickedButton = allButtons[i];
+
   clickedButton.onclick = function (_event) {
-    //console.log(clickedButton);
-    //displayInput = displayInput + this.innerHTML;
-    //console.log(this.innerHTML);
     clickedButtonValue = this.innerHTML;
-    console.log(clickedButtonValue);
-
-    //maximum length of the input number value = 10
-
+    //console.log(clickedButtonValue);
 
     switch (clickedButtonValue) { 
-
-      // Other Operators or special operators ' C ' , ' +/- ' ,  ' % '
-      case "C":  // COMPLETED
-        console.log("It is a c");
+    // ----------CANCEL------------------------
+      case "C":  
+        //console.log("It is a c");
         isDecimal = false;
         isNegative = false;
-        displayInput = "";
-         // clear all: ip, op, flags   
-         //?? clickedButtonValue = "";
-        displayInput = displayInput;
+        numberDisplay = "0";
+        calculationDisplay = "";
+        value1 = 0;
+        value2 = 0;
+        operator = '';          // clear all: ip, op, flags   
         break;
-
-      case "+/-": // COMPLETED
-        console.log("It is a +-");         // initialise flag &  // toggle flag
+    // ----------POSITIVE NEGATIVE------------- ' +/- '
+      case "+/-": 
+        //console.log("It is a +-");         // initialise flag &  // toggle flag
         if (isNegative === false) {
-          displayInput = '-' + displayInput;
+          numberDisplay = '-' + numberDisplay;
           isNegative = true;
         } else {
-          displayInput = displayInput.slice(1);
+          numberDisplay = numberDisplay.slice(1);
           isNegative = false;
         }
         break;
-
+    // ----------PERCENTAGE---------------------
       case "%": // TO DO
         console.log("It is a %");
-        displayInput +=  this.innerHTML;
         break;
-
-        // simple number operators  ' + ' , ' - ' ,  ' * ' , ' / ' , '  =  '
-
-        //let lastkey = displayInput.slice(-1) : function  when operator is pressed was the previous key an operator as well ? replace it then
-        // new numeric variable to hold the numbers keyed in before the operator button is pressed
-        // function to calculate the (reduce) calculation expression
-        // = symbol rules
-        //output display rules & variable updates acc to each function
-
+    //------------ ARITHMETIC OPERATIONS ------- ' + ' , ' - ' ,  ' * ' , ' / ' 
       case "+":
-        console.log("It is a +");
-        displayInput = displayInput + this.innerHTML;
-        break;
-
       case "-":
-        console.log("It is a -");
-        displayInput = displayInput + this.innerHTML;
-        break;
-
       case "x":
-        console.log("It is a x");
-        displayInput = displayInput + this.innerHTML;
-        break;
-
       case "÷":
-        console.log("It is a ÷");
-        displayInput = displayInput + this.innerHTML;
+        operator = this.innerHTML;
+        value1 = parseInt(numberDisplay);
+        calculationDisplay = numberDisplay + operator;
+        numberDisplay = "";
         break;
-
+    //------------ EQUAL TO --------------------- '  =  '
       case "=":
         console.log("It is a =");
-        displayOutput = displayInput;
-        break;
+        //move innerhtml into a variable, convert it to a number & call function to calculate
+          if (value1 != 0) {
+          value2 = parseInt(numberDisplay);
+          calculateFunction (value1, value2, operator);
+          operator = '';
+        }
+        else {
+          calculationDisplay = numberDisplay + '='
+          operator = '='
+         
+          //numberDisplay = 0
 
-      case ".": // COMPLETED
+        }
+                    
+        break;
+    //------------ DECIMAL POINT ------------------- '  .  '
+      case ".": 
         console.log("It is a ."); //initialise flag, set flag, if flag on : ignore
         if (isDecimal === false) {
-          if(displayInput === ''){displayInput = 0} // prefix 0 if there is no number before the decimal point
-            displayInput = displayInput + this.innerHTML;
+          if(numberDisplay === ''){numberDisplay = 0} // prefix 0 if there is no number before the decimal point
+          numberDisplay +=  this.innerHTML;
             isDecimal = true;
         }   
         break;
 
-         // NUMBERS ' 0 - 9 '
+    //------------- NUMBERS ' 0 - 9 ' --------------- 
 
       default:
-        console.log("It is a number");
-        //console.log(`check1 ${displayInput}`);
-        if (this.innerHTML === '0' && displayInput === '') {
-          displayInput = displayInput 
-         // console.log(`check2 ${displayInput}`);
-        } 
-        else { displayInput = displayInput + this.innerHTML
-         // console.log(`check3 ${displayInput}`);
-        };
-         // console.log(`check4 ${displayInput}`);
+
+          numberDisplay += this.innerHTML;
+          numberDisplay = parseInt(numberDisplay);
+
         break;
     }
 
-    document.getElementById("input").innerHTML = displayInput;
-    document.getElementById("output").innerHTML = displayOutput;
-    console.log(displayInput);
+
+
+
+
+
+
+    document.getElementById("numbers").innerHTML = numberDisplay;
+    document.getElementById("calculation").innerHTML = calculationDisplay;
+
   };
+      //------------------------------- 
+      //   function to calculate 
+      //------------------------------- 
+
+      const calculateFunction = (value1, value2, operator) => {
+        calculationDisplay = value1 + operator + value2;
+        switch (operator) {
+          case '+':
+            numberDisplay = value1 + value2;
+            break;
+          case '-':
+            numberDisplay = value1 - value2;
+            break;
+          case 'x':
+            numberDisplay = value1 * value2;
+            break;
+          case '÷':
+            numberDisplay = value1 / value2;
+            break;    
+        }
+        operator = ''
+      }
+
 }
+
+
